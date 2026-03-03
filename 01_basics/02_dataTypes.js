127e4d0f98190c81e8e4e7e5b35d35e334ddad57
himaanshu1702 <hsrajput1702@gmail.com>
2026-03-03 20:06:30 +0000

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use Strict"; // treat all js code as newer version
+
+// alert (3+3) //we are using nodejs , not browser
+
+console.log (3
+    +
+    3) // code readability should be high
+
+console.log ("himanshu")
+
+
+let name = "himasnhu"
+let age = 18
+let isloggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint (when no. is too big)
+// string =>""
+//boolean => true/false
+// null => standalone value 
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof "Himanshu") // string
+console.log(typeof age) // number
+console.log(typeof null) // object
+console.log(typeof undefined); // undefined
+
+
