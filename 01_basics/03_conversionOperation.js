c81bd7f0743968c1812f3c10047488a84fb61267
himaanshu1702 <hsrajput1702@gmail.com>
2026-03-04 12:06:36 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "himanshu"   // null = 0 , undefine = NaN , true = 1 
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)  // converting into a number 
+//console.log(typeof valueInNumber);  ===> // output = number
+//console.log(valueInNumber);  ==> // output = NaN
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "himanshu"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "himanshu" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
