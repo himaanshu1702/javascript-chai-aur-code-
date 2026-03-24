c5902de89a9fada68ba3cb1b10d3fb811df698e9
himaanshu1702 <hsrajput1702@gmail.com>
2026-03-24 19:37:03 +0000

    leture 21 scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,16 @@
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20                        //  {} inside it called Block scope and outside it {} called global scope 
+    // console.log("INNER: ", a);
+    
+}
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
